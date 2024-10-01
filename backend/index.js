4c7060e44f2ebfcd852f75b0bd44966f66296791
Onni <onni.vitikainen@tuni.fi>
2024-10-01 12:16:05 +0300

    Changed origin

--- backend/index.js
+++ backend/index.js
@@ -1,44 +1,35 @@
-const express = require("express"); // Import Express.js framework
-const cors = require("cors"); // Import CORS middleware
-const http = require("http"); // Import HTTP module to create server
-const dotenv = require("dotenv"); // Import dotenv to handle environment variables
+const express = require("express"); 
+const cors = require("cors"); 
+const http = require("http")
+const dotenv = require("dotenv");
 
-// Load environment variables from .env file
 dotenv.config();
 
-// Define the port to run the server, defaulting to 8888 if not set in environment variables
 const PORT = process.env.PORT || 8888;
 
-// Define CORS options for the server
 const corsOptions = {
-  origin: process.env.ORIGIN, // Allow requests only from the specified origin
-  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow these HTTP methods
-  allowedHeaders: ["Content-Type", "Authorization"], // Allow these headers in requests
-  credentials: true, // Allow cookies to be sent with requests
-  transports: ["websocket"], // Allow WebSocket transport for real-time communication
+  origin: process.env.ORIGIN,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], 
+  allowedHeaders: ["Content-Type", "Authorization"], 
+  credentials: true, 
+  transports: ["websocket"], 
 };
 
-// Create an instance of an Express application
 const app = express();
 
-// Use CORS middleware with the specified options
 app.use(cors(corsOptions));
-// Use middleware to parse JSON request bodies
 app.use(express.json());
 
-// Create an HTTP server using the Express app
 const server = http.createServer(app);
 
-// Import and initialize socket.io for real-time communication
 const initializeSocket = require("./sockets/socket");
 initializeSocket(server, {
   cors: {
-    origin: process.env.ORIGIN, // Allow requests only from the specified origin
-    methods: ["GET", "POST"], // Allow these HTTP methods
+    origin: process.env.ORIGIN,
+    methods: ["GET", "POST"], 
   },
 });
 
-// Middleware to set CORS headers for responses
 app.use((req, res, next) => {
   const origin = req.headers.origin;
   if (origin === process.env.ORIGIN) {
@@ -48,7 +39,6 @@ app.use((req, res, next) => {
   next();
 });
 
-// Start the server and listen on the specified port
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
